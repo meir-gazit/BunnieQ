65e0565621fa212d75c3f53e3dd5af7e4453bd51
User <User@GAZIT-NETWORK>
2022-09-28 13:43:38 +0300

    Add project files.

---
+++ Patite.Domain/Bus/IEventBus.cs
@@ -0,0 +1,19 @@
+﻿using Patite.Domain.Commands;
+using Patite.Domain.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patite.Domain.Bus
+{
+    public interface IEventBus
+    {
+        Task SendCommand<T>(T command) where T : Command;
+        void Publish<T>(T @event) where T : Event;
+        void Subscribe<T, THandler>() where T : Event where THandler : IEventHandler<T>;
+
+    }
+}
---
+++ Patite.Domain/Commands/Command.cs
@@ -0,0 +1,19 @@
+﻿using Patite.Domain.Events;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patite.Domain.Commands
+{
+    public abstract class Command : Message
+    {
+        public DateTime TimeStamp { get; protected set; }
+        protected Command()
+        {
+            TimeStamp = DateTime.Now;
+        }
+    }
+}
