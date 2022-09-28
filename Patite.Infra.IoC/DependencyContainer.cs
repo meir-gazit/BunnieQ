8550c8d2588bd5c5d64a17ed06cf15e7f67b8f9b
meir-gazit <emgazit@gmail.com>
2022-09-28 20:46:47 +0300

    init infracontainner

---
+++ Patite.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Patite.Domain.Bus;
+using Patite.Infra.Bus;
+
+namespace Patite.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterService(IServiceCollection services)
+        {
+            //Domain Bus
+            services.AddTransient<IEventBus, RabbitMqBus>();
+        }
+    }
+}
